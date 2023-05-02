ead77101f701e9b369c5c053780ee2112c73c9de
Vadym Panchenko <panchenko.vadym@gmail.com>
2023-05-02 19:04:37 +0300

    Added example of `TextInput`

--- DetailsScreen.tsx
+++ DetailsScreen.tsx
@@ -1,23 +1,45 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import { Button } from 'react-native-paper';
 import { useTheme } from '@react-navigation/native';
 
 const DetailsScreen = ({ navigation, route }): JSX.Element => {
   const colors = useTheme().colors;
+  const [name, setName] = useState('');
 
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'space-between',
         backgroundColor: colors.background
       }}
     >
       <Text style={{ color: colors.text }}>
         Details Screen has parameter `name` {route.params.name}
       </Text>
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2ea9a9',
+      }}>
+        <Text style={{ color: colors.text }}>
+          Please type your name:
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder={'e.g. Nicolas'}
+          onChangeText={(value) => setName(value)}
+          keyboardType={'name-phone-pad'}
+          maxLength={25}
+          editable={true}
+          secureTextEntry={false}
+        />
+        <Text style={{color: colors.text}}>Your name is: {name}</Text>
+      </View>
       <Button
         mode="contained"
         onPress={() => navigation.navigate('Test', { prop1: 'value1' })}
@@ -28,4 +50,15 @@ const DetailsScreen = ({ navigation, route }): JSX.Element => {
   );
 };
 
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    width: 200,
+    borderColor: "#555",
+    borderRadius: 5,
+    textAlign: "center",
+    fontSize: 20,
+  }
+})
 export default DetailsScreen;
