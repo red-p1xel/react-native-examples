11a7785196d50e9fabb739b71c3abffa113a4e5e
Vadym Panchenko <panchenko.vadym@gmail.com>
2023-05-02 19:58:59 +0300

    Button, Touchables and Pressable

--- DetailsScreen.tsx
+++ DetailsScreen.tsx
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 import React, { useState } from 'react';
 import { Button } from 'react-native-paper';
 import { useTheme } from '@react-navigation/native';
@@ -6,6 +15,10 @@ import { useTheme } from '@react-navigation/native';
 const DetailsScreen = ({ navigation, route }): JSX.Element => {
   const colors = useTheme().colors;
   const [name, setName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const onPressHandler = () => {
+    setSubmitted(!submitted);
+  };
 
   return (
     <View
@@ -20,15 +33,15 @@ const DetailsScreen = ({ navigation, route }): JSX.Element => {
       <Text style={{ color: colors.text }}>
         Details Screen has parameter `name` {route.params.name}
       </Text>
-      <View style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#2ea9a9',
-      }}>
-        <Text style={{ color: colors.text }}>
-          Please type your name:
-        </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#2ea9a9'
+        }}
+      >
+        <Text style={{ color: colors.text }}>Please type your name:</Text>
         <TextInput
           style={styles.input}
           placeholder={'e.g. Nicolas'}
@@ -38,8 +51,65 @@ const DetailsScreen = ({ navigation, route }): JSX.Element => {
           editable={true}
           secureTextEntry={false}
         />
-        <Text style={{color: colors.text}}>Your name is: {name}</Text>
+
+        <Button
+          mode={'contained-tonal'}
+          onPress={onPressHandler}
+          disabled={submitted}
+        >
+          {submitted ? 'Clear' : 'Submit'}
+        </Button>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPressHandler}
+          activeOpacity={0.5}
+        >
+          <Text style={{ color: colors.text, margin: 10, fontSize: 20 }}>
+            {submitted ? 'Clear' : 'Submit TO'}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={onPressHandler}
+          activeOpacity={0.5}
+          underlayColor={'#47491b'}
+        >
+          <Text style={{ color: colors.text, margin: 10, fontSize: 20 }}>
+            {submitted ? 'Clear' : 'Submit TH'}
+          </Text>
+        </TouchableHighlight>
+
+        <TouchableWithoutFeedback
+          style={styles.button}
+          onPress={onPressHandler}
+        >
+          <Text style={{ color: colors.text, margin: 10, fontSize: 20 }}>
+            {submitted ? 'Clear' : 'Submit WF'}
+          </Text>
+        </TouchableWithoutFeedback>
+
+        <Pressable
+          onPress={onPressHandler}
+          android_ripple={{ color: '#ff0000' }}
+          style={({ pressed }) => [
+            styles.button,
+            { backgroundColor: pressed ? '#97fa00' : '#177e17' }
+          ]}
+        >
+          <Text style={[{ color: colors.text }, styles.text]}>
+            {submitted ? 'Clear' : 'Submit'}
+          </Text>
+        </Pressable>
+
+        {submitted ? (
+          <Text style={{ color: colors.text, margin: 10 }}>
+            You are registered: {name}
+          </Text>
+        ) : null}
       </View>
+
       <Button
         mode="contained"
         onPress={() => navigation.navigate('Test', { prop1: 'value1' })}
@@ -51,14 +121,25 @@ const DetailsScreen = ({ navigation, route }): JSX.Element => {
 };
 
 const styles = StyleSheet.create({
+  text: {
+    margin: 15,
+    fontSize: 15
+  },
   input: {
     backgroundColor: '#fff',
     borderWidth: 1,
     width: 200,
-    borderColor: "#555",
+    borderColor: '#555',
     borderRadius: 5,
-    textAlign: "center",
-    fontSize: 20,
+    textAlign: 'center',
+    fontSize: 20
+  },
+  button: {
+    backgroundColor: '#177e17',
+    width: 150,
+    height: 50,
+    alignItems: 'center'
   }
-})
+});
+
 export default DetailsScreen;
